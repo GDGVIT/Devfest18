68cfc0db824bc8967c0a2ece7e04fc46645c5e8f
technophilic <svineth.face@gmail.com>
2018-05-25 00:32:04 +0530

    First section Done

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import 'hammerjs';
+import 'materialize-css/dist/js/materialize';
+
+$(function () {
+    console.log('jquery',$);
+    console.log('materialize',Materialize);
+    // ------ Scripts here --------
+});
